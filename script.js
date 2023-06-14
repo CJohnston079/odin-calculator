aaeed21e155da0c6c2f6fe878e2e8b7c29ec8dd8
Chris <chris79gp@outlook.com>
2023-06-14 17:15:32 +0100

    renamed variables from numberKeys et al to numberButtons et al, for clarity when using for... in loops

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 const calculationDisplay = document.querySelector('#calculation')
 
-const functionKeyElements = document.querySelector('#function-panel').querySelectorAll('.button');
-const numberKeyElements = document.querySelector('#numpad').querySelectorAll('.button');
-const operationKeyElements = document.querySelectorAll('.operation');
+const functionButtonElements = document.querySelector('#function-panel').querySelectorAll('.button');
+const numberButtonElements = document.querySelector('#numpad').querySelectorAll('.button');
+const operationButtonElements = document.querySelectorAll('.operation');
 
-const functionKeys = {
-    equals: operationKeyElements[6],
-    clear: operationKeyElements[1],
-    allClear: operationKeyElements[0],
-    memoryClear: functionKeyElements[0],
-    memoryAdd: functionKeyElements[1],
-    memorySubtract: functionKeyElements[2],
-    memoryRecall: functionKeyElements[3],
-    brackets: functionKeyElements[4],
-    pi: functionKeyElements[5],
-    factorial: functionKeyElements[6],
-    percent: functionKeyElements[7],
-    power: functionKeyElements[8],
-    root: functionKeyElements[9]
+const functionButtons = {
+    equals: operationButtonElements[6],
+    clear: operationButtonElements[1],
+    allClear: operationButtonElements[0],
+    memoryClear: functionButtonElements[0],
+    memoryAdd: functionButtonElements[1],
+    memorySubtract: functionButtonElements[2],
+    memoryRecall: functionButtonElements[3],
+    brackets: functionButtonElements[4],
+    pi: functionButtonElements[5],
+    factorial: functionButtonElements[6],
+    percent: functionButtonElements[7],
+    power: functionButtonElements[8],
+    root: functionButtonElements[9]
 }
 
-const numberKeys = {
-    '0': { element: numberKeyElements[10], value: 0 },
-    '1': { element: numberKeyElements[0], value: 1 },
-    '2': { element: numberKeyElements[1], value: 2 },
-    '3': { element: numberKeyElements[2], value: 3 },
-    '4': { element: numberKeyElements[3], value: 4 },
-    '5': { element: numberKeyElements[4], value: 5 },
-    '6': { element: numberKeyElements[5], value: 6 },
-    '7': { element: numberKeyElements[6], value: 7 },
-    '8': { element: numberKeyElements[7], value: 8 },
-    '9': { element: numberKeyElements[8], value: 9 },
+const numberButtons = {
+    '0': { element: numberButtonElements[10], value: 0 },
+    '1': { element: numberButtonElements[0], value: 1 },
+    '2': { element: numberButtonElements[1], value: 2 },
+    '3': { element: numberButtonElements[2], value: 3 },
+    '4': { element: numberButtonElements[3], value: 4 },
+    '5': { element: numberButtonElements[4], value: 5 },
+    '6': { element: numberButtonElements[5], value: 6 },
+    '7': { element: numberButtonElements[6], value: 7 },
+    '8': { element: numberButtonElements[7], value: 8 },
+    '9': { element: numberButtonElements[8], value: 9 },
 }
 
-const operationKeys = {
-    add: { element: operationKeyElements[2], value: '+' },
-    subtract: { element: operationKeyElements[3], value: '-' },
-    multiply: { element: operationKeyElements[4], value: '*' },
-    divide: { element: operationKeyElements[5], value: '/' }
+const operationButtons = {
+    add: { element: operationButtonElements[2], value: '+' },
+    subtract: { element: operationButtonElements[3], value: '-' },
+    multiply: { element: operationButtonElements[4], value: '*' },
+    divide: { element: operationButtonElements[5], value: '/' }
 }
 
-let calculation = 0;
+let calculation = '';
 let history = [];
 
 function updateMainDisplay() {
     calculationDisplay.textContent = calculation;
 }
 
-function updateCalculation(value) {
-    calculation += value;
+function updateCalculation(button) {
+    calculation += button.element.textContent;
     console.log(calculation);
     return calculation
 }
@@ -56,23 +56,23 @@ function updateCalculation(value) {
 function inputEquation(object) {
     for (const key in object) {
         object[key].element.addEventListener('mousedown', () => {
-            updateCalculation(object[key].value);
+            updateCalculation(object[key]);
             updateMainDisplay();
         });
     } 
 }
 
-inputEquation(numberKeys);
-inputEquation(operationKeys);
+inputEquation(numberButtons);
+inputEquation(operationButtons);
 
-functionKeys.equals.addEventListener('mousedown', calculate);
+functionButtons.equals.addEventListener('mousedown', calculate);
 
 function calculate() {
     console.log('calculating...')
     history.push(calculation);
 }
 
-functionKeys.clear.addEventListener('mousedown', () => {
+functionButtons.clear.addEventListener('mousedown', () => {
     clearCharacter(),
     updateMainDisplay();
 })
@@ -82,7 +82,7 @@ function clearCharacter() {
     return calculation
 }
 
-functionKeys.allClear.addEventListener('mousedown', () => {
+functionButtons.allClear.addEventListener('mousedown', () => {
     calculation = 0;
     updateMainDisplay();
     return calculation
