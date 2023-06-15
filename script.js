27b5136aa1a22f83284048bbf0d5b7adb84d6291
Chris <chris79gp@outlook.com>
2023-06-15 13:03:54 +0100

    numbers are now added to the array one character at a time, to allow for the clear character function to work efficiently

--- script.js
+++ script.js
@@ -41,9 +41,9 @@ const operationButtons = {
 }
 
 let equation = '';
-let history = [];
 let equationArray = [];
-let previousNumberSlice = 0;
+let history = [];
+// let previousNumberSlice = 0;
 
 function updateMainDisplay() {
     if (equation === '') {
@@ -55,26 +55,39 @@ function updateMainDisplay() {
 
 function updateEquation(object, key) {
     if (object === operationButtons && isNaN(Number(equation[equation.length-1])) === true) return 
+    
     equation += object[key].value;
 
-    if (object === operationButtons) {
-        pushNumber();
-        pushOperation();
-    }
+    pushCharacter()
+
+    // if (object === operationButtons) {
+    //     pushNumber();
+    //     pushOperation();
+    // }
 
     return equation
 }
 
-function pushNumber() {
-    equationArray.push(Number(equation.slice(previousNumberSlice, equation.length-1)));
-    previousNumberSlice = equation.length;
+function pushCharacter() {
+    if (isNaN(Number(equation[equation.length-1])) === true) {
+        equationArray.push(equation[equation.length-1]);
+    } else {
+        equationArray.push(Number(equation[equation.length-1]));
+    }
+    
     console.log(equationArray)
 }
 
-function pushOperation() {
-    equationArray.push(equation[equation.length-1])
-    console.log(equationArray)
-}
+// function pushNumber() {
+//     equationArray.push(Number(equation.slice(previousNumberSlice, equation.length-1)));
+//     previousNumberSlice = equation.length;
+//     console.log(equationArray)
+// }
+
+// function pushOperation() {
+//     equationArray.push(equation[equation.length-1])
+//     console.log(equationArray)
+// }
 
 function inputEquation(object) {
     for (const key in object) {
@@ -88,12 +101,13 @@ function inputEquation(object) {
 inputEquation(numberButtons);
 inputEquation(operationButtons);
 
+functionButtons.equals.addEventListener('mousedown', pushCharacter);
 functionButtons.equals.addEventListener('mousedown', calculate);
 
 function calculate() {
     if (isNaN(Number(equation[equation.length-1])) === true) return
-    pushNumber()
     console.log('calculating...')
+    clearEquationArray()
     updateHistory()
 }
 
@@ -110,18 +124,26 @@ function clearHistory() {
     return history
 }
 
+function clearEquationArray() {
+    equationArray = [];
+    return equationArray
+}
+
 functionButtons.clear.addEventListener('mousedown', () => {
     clearCharacter(),
     updateMainDisplay();
 })
 
 function clearCharacter() {
-    equation = equation.slice(0,-1)
+    equation = equation.slice(0,-1);
+    equationArray.pop();
+    console.log(equationArray)
     return equation
 }
 
 functionButtons.allClear.addEventListener('mousedown', () => {
     equation = '';
+    clearEquationArray();
     clearHistory();
     updateMainDisplay();
     return equation
